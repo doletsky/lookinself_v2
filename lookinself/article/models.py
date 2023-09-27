b7d3b398cbdbdf0a82c69cabab8781316999baa1
Alexander Doletsky <doletsky@bk.ru>
2023-09-27 20:59:27 +0300

    add Article Model

--- lookinself/article/models.py
+++ lookinself/article/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="uploads/article/")
+    short = models.TextField()
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    sort = models.IntegerField(default=1)
+    test = models.IntegerField(default=None)
+    image_a = models.ImageField(upload_to="uploads/article/")
+    image_b = models.ImageField(upload_to="uploads/article/")
+    image_c = models.ImageField(upload_to="uploads/article/")
+    image_d = models.ImageField(upload_to="uploads/article/")
+    image_f = models.ImageField(upload_to="uploads/article/")
 # Create your models here.
--- lookinself/lookinself/settings.py
+++ lookinself/lookinself/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = "templates/static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = f'{BASE_DIR}/templates/images'
